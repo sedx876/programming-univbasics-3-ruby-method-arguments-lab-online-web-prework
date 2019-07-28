8b949489ef9fb8c7f4c62c0ed82491a060c305ae
Sharon Watkins <swatkins876@gmail.com>
2019-07-28 14:25:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ end
 def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language_optional("name", "Ruby")
+introduction_with_language_optional("Edwin")
 
 def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
