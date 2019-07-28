898a29bb3da92ada562203520e12b924c3a400e0
Sharon Watkins <swatkins876@gmail.com>
2019-07-28 14:06:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,18 +1,6 @@
-def introduction(name)
-  puts "Hi, my name is #{name}\n"
-end
 
-introduction("Josh")
-
-def introduction(name)
-  puts "Hi, my name is #{name}\n"
-end
-
-introduction("Dan")
-
-name = "Josh"
-introduction(name)
 
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
