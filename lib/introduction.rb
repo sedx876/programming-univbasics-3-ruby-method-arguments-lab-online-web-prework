dd250337716037943f32a88f4596f0380ba25d51
Sharon Watkins <swatkins876@gmail.com>
2019-07-28 13:48:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,5 +2,8 @@ name = "Josh"
 puts "Hi my name is #{name}"
 
 name = "Dan"
+puts "Hi name is #{name}"
+
+name = "Dan"
 language = "Ember.js"
 puts "Hi, my name is #{name} and I am learning to program in #{language}\n"
